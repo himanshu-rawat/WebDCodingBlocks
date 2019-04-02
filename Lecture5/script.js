dc2fd73230b3250527cd5b7bc61a313bae672c1d
Himanshu Rawat <himanshurawatrit@gmail.com>
2019-04-02 16:58:46 +0530

    Lecture 5

---
+++ Lecture5/script.js
@@ -0,0 +1,16 @@
+console.log('Hi I am Here');
+
+let x = 'Himanshu';
+console.log(x);
+
+// const y=13;  This cannot be done...
+// y=32;
+const obj = { a: 23, name: 'Himanshu', college: true };
+console.log(obj);
+obj.a = 20;
+console.log(obj);
+delete obj.name;
+console.log(obj);
+
+obj = { sdf: 1345 };
+console.log(obj);
