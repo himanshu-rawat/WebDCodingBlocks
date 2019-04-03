b4ad286c8ffe8e471b4a319a2125124e40e461ed
Himanshu Rawat <himanshurawatrit@gmail.com>
2019-04-04 01:33:55 +0530

    Lecture About JavaScript Varible and DOM

--- Lecture5/script.js
+++ Lecture5/script.js
@@ -1,23 +1,23 @@
-console.log('Hi I am Here');
+// console.log('Hi I am Here');
 
-let x = 'Himanshu';
-console.log(x);
+// let x = 'Himanshu';
+// console.log(x);
 
-// const y=13;  This cannot be done...
-// y=32;
-const obj = { a: 23, name: 'Himanshu', college: true };
-console.log(obj);
-obj.a = 20;
-console.log(obj);
-delete obj.name;
-console.log(obj);
+// // const y=13;  This cannot be done...
+// // y=32;
+// const obj = { a: 23, name: 'Himanshu', college: true };
+// console.log(obj);
+// obj.a = 20;
+// console.log(obj);
+// delete obj.name;
+// console.log(obj);
 
-// obj = { sdf: 1345 };  this cannot be done because it give differnt refernces to the object.
-console.log(obj);
+// // obj = { sdf: 1345 };  this cannot be done because it give differnt refernces to the object.
+// console.log(obj);
 
-function sayHello(name) {
-	console.log('Hello ' + name);
-}
-sayHello('Aman');
-sayHello();
-sayHello('Ram ', 'sysf');
+// function sayHello(name) {
+// 	console.log('Hello ' + name);
+// }
+// sayHello('Aman');
+// sayHello();
+// sayHello('Ram ', 'sysf');
